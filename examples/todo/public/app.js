7b3a451fc49c61719382b08afa24408e67b66a14
Amal Shehu <amalshehu@ashmxmz-titan.local>
2019-02-11 23:28:47 +0530

    Add firebase

---
+++ examples/todo/public/app.js
@@ -0,0 +1,18 @@
+import { R, H } from 'https://cdn.jsdelivr.net/gh/santoshrajan/simba/simba.js';
+
+import addNew from './comps/addnew.js';
+import createTodo from './comps/todo.js';
+
+import Todos from './comps/todos.js';
+
+/* Render to #App */
+
+R(
+  H('h1', { className: 'title is-1' }, 'Todo'),
+  H(addNew, { className: 'field' }),
+  H('p', { className: 'help' }, 'Double click todo below to edit'),
+  Todos(state => {
+    state.todos = [];
+  }),
+  document.getElementById('App')
+);
